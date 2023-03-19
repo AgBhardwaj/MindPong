4ca50d26b0a2892aca2c32d6a1e5d67bfddbdad3
AgBhardwaj <127640504+AgBhardwaj@users.noreply.github.com>
2023-03-19 11:47:24 -0700

    Add files via upload

---
+++ pong.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+"""
+Estimate Relaxation from Band Powers
+
+This example shows how to buffer, epoch, and transform EEG data from a single
+electrode into values for each of the classic frequencies (e.g. alpha, beta, theta)
+Furthermore, it shows how ratios of the band powers can be used to estimate
+mental state for neurofeedback.
+
+The neurofeedback protocols described here are inspired by
+*Neurofeedback: A Comprehensive Review on System Design, Methodology and Clinical Applications* by Marzbani et. al
+
+Adapted from https://github.com/NeuroTechX/bci-workshop
+"""
+
+import numpy as np  # Module that simplifies computations on matrices
+import matplotlib.pyplot as plt  # Module used for plotting
+from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
+import utils  # Our own utility functions
+
+import turtle
+import os
+# Handy little enum to make code more readable
+
+
+class Band:
+    Delta = 0
+    Theta = 1
+    Alpha = 2
+    Beta = 3
+
+
+def ballMove(delta_brainwaves, ballDirection):
+  if delta_brainwaves > 1.3:
+    print("You Blinked")
+    if ballDirection == 1:
+      ballDirection = 2
+      return ballDirection
+    elif ballDirection == 2:
+      ballDirection = 3
+      return ballDirection
+    elif ballDirection == 3:
+      ballDirection = 1
+      return ballDirection
+  else:
+    return ballDirection
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 10
+ball.dy = 10
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+
+# Functions
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+
+wn.listen()
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+
+""" EXPERIMENTAL PARAMETERS """
+# Modify these to change aspects of the signal processing
+
+# Length of the EEG data buffer (in seconds)
+# This buffer will hold last n seconds of data and be used for calculations
+BUFFER_LENGTH = 5
+
+# Length of the epochs used to compute the FFT (in seconds)
+EPOCH_LENGTH = 1
+
+# Amount of overlap between two consecutive epochs (in seconds)
+OVERLAP_LENGTH = 0.3
+
+# Amount to 'shift' the start of each next consecutive epoch
+SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+ballDirection = 1
+# Index of the channel(s) (electrodes) to be used
+# 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
+INDEX_CHANNEL = [0]
+
+if __name__ == "__main__":
+
+    """ 1. CONNECT TO EEG STREAM """
+
+    # Search for active LSL streams
+    print('Looking for an EEG stream...')
+    streams = resolve_byprop('type', 'EEG', timeout=2)
+    if len(streams) == 0:
+        raise RuntimeError('Can\'t find EEG stream.')
+
+    # Set active EEG stream to inlet and apply time correction
+    print("Start acquiring data")
+    inlet = StreamInlet(streams[0], max_chunklen=12)
+    eeg_time_correction = inlet.time_correction()
+
+    # Get the stream info and description
+    info = inlet.info()
+    description = info.desc()
+
+    # Get the sampling frequency
+    # This is an important value that represents how many EEG data points are
+    # collected in a second. This influences our frequency band calculation.
+    # for the Muse 2016, this should always be 256
+    fs = int(info.nominal_srate())
+
+    """ 2. INITIALIZE BUFFERS """
+
+    # Initialize raw EEG data buffer
+    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+    filter_state = None  # for use with the notch filter
+
+    # Compute the number of epochs in "buffer_length"
+    n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
+                              SHIFT_LENGTH + 1))
+
+    # Initialize the band power buffer (for plotting)
+    # bands will be ordered: [delta, theta, alpha, beta]
+    band_buffer = np.zeros((n_win_test, 4))
+
+    """ 3. GET DATA """
+
+    # The try/except structure allows to quit the while loop by aborting the
+    # script with <Ctrl-C>
+    print('Press Ctrl-C in the console to break the while loop.')
+
+    try:
+        # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
+        while True:
+
+             #   Update the Pong Game
+            wn.update()
+            
+            # Move the ball
+            ball.setx(ball.xcor() + ball.dx)
+            ball.sety(ball.ycor() + ball.dy)
+
+            # Border checking
+
+            # Top and bottom
+            if ball.ycor() > 290:
+                ball.sety(290)
+                ball.dy *= -1
+            
+            elif ball.ycor() < -290:
+                ball.sety(-290)
+                ball.dy *= -1
+
+            # Left and right
+            if ball.xcor() > 350:
+                score_a += 1
+                pen.clear()
+                pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                ball.goto(0, 0)
+                ball.dx *= -1
+
+            elif ball.xcor() < -350:
+                score_b += 1
+                pen.clear()
+                pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+                ball.goto(0, 0)
+                ball.dx *= -1
+
+            # Paddle and ball collisions
+            if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+                ball.dx *= -1 
+            
+            elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+                ball.dx *= -1
+            """ 3.1 ACQUIRE DATA """
+            # Obtain EEG data from the LSL stream
+            eeg_data, timestamp = inlet.pull_chunk(
+                timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+
+            # Only keep the channel we're interested in
+            ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
+
+            # Update EEG buffer with the new data
+            eeg_buffer, filter_state = utils.update_buffer(
+                eeg_buffer, ch_data, notch=True,
+                filter_state=filter_state)
+
+            """ 3.2 COMPUTE BAND POWERS """
+            # Get newest samples from the buffer
+            data_epoch = utils.get_last_data(eeg_buffer,
+                                             EPOCH_LENGTH * fs)
+
+            # Compute band powers
+            band_powers = utils.compute_band_powers(data_epoch, fs)
+            band_buffer, _ = utils.update_buffer(band_buffer,
+                                                 np.asarray([band_powers]))
+            # Compute the average band powers for all epochs in buffer
+            # This helps to smooth out noise
+            smooth_band_powers = np.mean(band_buffer, axis=0)
+
+            # print('Delta: ', band_powers[Band.Delta], ' Theta: ', band_powers[Band.Theta],
+            #       ' Alpha: ', band_powers[Band.Alpha], ' Beta: ', band_powers[Band.Beta])
+
+            """ 3.3 COMPUTE NEUROFEEDBACK METRICS """
+            # These metrics could also be used to drive brain-computer interfaces
+
+            # Alpha Protocol:
+            # Simple redout of alpha power, divided by delta waves in order to rule out noise
+            delta_brainwaves = band_powers[Band.Delta]
+            print("Delta Brainwaves: ", delta_brainwaves)
+            ballDirection = ballMove(delta_brainwaves, ballDirection)
+            if ballDirection == 1:
+               paddle_a_up()
+            if ballDirection == 3:
+               paddle_a_down()
+
+            # Beta Protocol:
+            # Beta waves have been used as a measure of mental activity and concentration
+            # This beta over theta ratio is commonly used as neurofeedback for ADHD
+            # beta_metric = smooth_band_powers[Band.Beta] / \
+            #     smooth_band_powers[Band.Theta]
+            # print('Beta Concentration: ', beta_metric)
+
+            # Alpha/Theta Protocol:
+            # This is another popular neurofeedback metric for stress reduction
+            # Higher theta over alpha is supposedly associated with reduced anxiety
+            # theta_metric = smooth_band_powers[Band.Theta] / \
+            #     smooth_band_powers[Band.Alpha]
+            # print('Theta Relaxation: ', theta_metric)
+
+    except KeyboardInterrupt:
+        print('Closing!')
